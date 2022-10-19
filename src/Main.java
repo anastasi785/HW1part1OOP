d0326ce902c316b4f32276fb79d89a112f42611d
nastu <nastushik92@bk.ru>
2022-10-19 19:48:55 +0300

    1

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Car lada=new Car("Lada","Granta","Россия","желтый",1.7,2015);
+        lada.information();
+        Car audi=new Car("Audi","50 L TDL quatro","Германия","чёрный",3.0,2020);
+        audi.information();
+        Car bmw=new Car("BMW","Z8","Германия","черный",3.0,2021);
+        bmw.information();
+        Car kia=new Car("Kia","Sportage","Южная Корея","красный",2.4,2018);
+        kia.information();
+        Car hunday=new Car("Hunday","Avante","Южная Корея","оранжевый",1.6,2016);
+        hunday.information();
+
+    }
+}
